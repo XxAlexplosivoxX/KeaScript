b69905899b32155866a9da0679ae94036add84cb
XxAlex_plosivoxX <Alex_plosivo_777@proton.me>
2025-11-06 20:56:42 -0600

    MODIFICATED

--- KeaScript.sh
+++ KeaScript.sh
@@ -16,35 +16,37 @@ errorMsj() { # mostrar mensaje de error con cuenta regresiva
 
 # verificar si es NO super usuario (Effective User ID -not equal- 0) para mostrar mensaje de alerta
 [[ $EUID -ne 0 ]] && { echo -e "${rojo}Debes ser superusuario pibe, deja de cagar!!"; exit 1; }
-
-echo -e "${cyan}Qué querés hacer pibe?:$reset"
-select eleccion in salir "actualizar repositorios" "instalar dependencias" "configurar pasarela"; do
-	case $eleccion in
-		salir)
-			echo -e "${verde}vale bro, adiós...${reset}"
-			exit 0
-			;;
-		configurar*)
-			
-			;;
-		instalar*)
-			
-			;;
-		actualizar*)
-			echo -en "${verde}actualizando repo...$reset"
-			apt update &> /dev/null
-			echo -en "${verde}actualizando repo... Listo :D$reset"
-			echo -en "Querés también instalar actualizaciones de paquetes?(s/n): $cyan"
-			read siono
-			if [[ "$siono" =~ ^[Ss]$ ]]; then
-				echo -e "${verde}vale, actualizando...$reset"
-				export DEBIAN_FRONTEND=noninteractive
-				apt upgrade -y &> /dev/null
-			elif [[ "$siono" =~ ^[Nn]$ ]]; then
-				echo -e "${verde}vale, no actualizo...$reset"
-			fi
-			;;
-		*)
-			errorMsj "colocá algo válido pibe, no seas así..."
-	esac
+while true; do
+	echo -e "${cyan}Qué querés hacer pibe?:$reset"
+	select eleccion in salir "actualizar repositorios" "instalar dependencias" "configurar pasarela"; do
+		case $eleccion in
+			salir)
+				echo -e "${verde}vale bro, adiós...${reset}"
+				exit 0
+				;;
+			configurar*)
+				
+				;;
+			instalar*)
+				
+				;;
+			actualizar*)
+				echo -en "${verde}actualizando repo...$reset"
+				apt update &> /dev/null
+				echo -en "${rline}${verde}actualizando repo... Listo :D$reset\n"
+				echo -en "Querés también instalar actualizaciones de paquetes?(s/n): $cyan"
+				read siono
+				if [[ "$siono" =~ ^[Ss]$ ]]; then
+					echo -e "${verde}vale, actualizando...$reset"
+					export DEBIAN_FRONTEND=noninteractive
+					apt upgrade -y &> /dev/null
+				elif [[ "$siono" =~ ^[Nn]$ ]]; then
+					echo -e "${verde}vale, no actualizo...$reset"
+				fi
+				break
+				;;
+			*)
+				errorMsj "colocá algo válido pibe, no seas así..."
+		esac
+	done
 done
